ec6ca29456f92dbcc0f498c33193d15b57d7c44e
anandubale <au26199@gmail.com>
2022-02-13 09:23:00 +0530

    Checking dublicate contact using details.some arrow function

--- AddressBook.js
+++ AddressBook.js
@@ -130,3 +130,15 @@ function getCount(details){
 details.reduce(getCount,1);
 console.log("**********************************************************");
 console.log("Total Contacts : " + contactDetails);
+
+
+//Dublicate contact:
+
+let personContact = new AddressBook("Christopher","Forst","2846  Tori Lane","Saltlakecity","Utah","459867","91 8015870002","ctmgz50esj@temporarymail.net");
+if(details.some(e => e._lastName == "Forst"))
+    console.log("Contact already Exists!");
+else{
+    details.push(personContact);
+    console.log("Contact added succsefully");
+}
+console.log("Array: ",details);
